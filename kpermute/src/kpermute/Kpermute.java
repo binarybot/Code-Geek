b4861f57dbf794d289c10f5db7b000b801ef340e
Mohan Nataraj <mnatara3@hawk.iit.edu>
2014-03-22 01:32:06 -0500

    Permutation of String for a given size K

---
+++ kpermute/src/kpermute/Kpermute.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kpermute;
+
+import java.util.*;
+
+/**
+ *
+ * @author Mohan
+ */
+public class Kpermute {
+    
+    public static void permutekUtil(String s2[],String prefix,int n,int k){
+    
+    
+        if(k==0){System.out.println(prefix);
+        return;
+        }
+        Vector<Character> c = new Vector<>();
+        for(int i=0;i<n;i++){
+        
+          String   nprefix = prefix + s2[i];
+             
+             permutekUtil(s2,nprefix,n,k-1);
+        }
+        
+        
+    }
+
+    public static void permutek(String s2[], int k){
+    
+        int n = s2.length;
+        
+        permutekUtil(s2,"",n,k);
+    
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        String sa = sc.nextLine();
+        
+        String s1[] = sa.split("\\s");
+        int k =2;
+        permutek(s1,k);
+        
+    }
+}
